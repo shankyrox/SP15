d3fa828a33674edd75d512c5c4c2cde53d8cbee2
svadivu <sanmugavadivu@gmail.com>
2016-12-04 17:58:51 +0530

    Add files via upload

---
+++ dbg.c
@@ -0,0 +1,139 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<time.h>
+#include<string.h>
+#include<execinfo.h>
+#include "debug.h"
+
+char* get_curr_time ()
+{
+    time_t rawtime;
+    struct tm *timeinfo;
+    time ( &rawtime );
+    timeinfo = localtime ( &rawtime );
+    //printf ( "Current local time and date: %s", asctime (timeinfo) );
+    return(asctime(timeinfo));
+}
+
+void create_log_file (file) 
+{
+   /*gets created at the run time if the flag is mentioned
+    uses current working dir to create the files */
+    snprintf(file , "_%s_syslog", get_curr_time());
+    fptr = fopen(file , "w");
+    return ;
+}
+
+void delete_log_file ()
+{
+   if (!fptr) {
+      printf ("\n Null pointer..can't delete the file\n");
+      return ;
+    }
+   fclose(fptr);
+   int ret = remove(file);
+   if (ret == 0)
+      printf("\n File %s deleted successfully ", file);
+   return ;
+}
+
+void write_to_logfile(char  *msg) {
+   if (!fptr) {
+     printf ("\n NULL pointer..can't write into the file");
+     return ;  
+   }
+  fprintf(fptr,"%s",msg);
+  return ;
+}
+
+//fill it in later
+void print_func_stack() {
+   void *arr[10];
+   size_t size, i;
+   char **fname;
+   char *tmp_str;
+   strcpy(tmp_str , "\nTraceback:");
+   size = backtrace(arr,10);
+   fname = backtrace_symbols(arr,size);
+   for (i=0; i<size ; i++ ) {
+       snprintf(tmp_str , 1000 ,fname[i]);
+     }    
+   printf("%s", tmp_str);
+   if (to_file) {
+     write_to_logfile(tmp_str);
+   }
+return;
+}
+
+//fill it in later
+void kill_the_proc() {
+ return;
+}
+
+void print_sys_log(char *msg, log_level var ) 
+{
+   char *tmp_str;
+   int pid=0; // get the pid later
+   //check if msg contains all fields necessary later
+   if (!msg) {
+      printf("\n Null string returned for log message \n");
+      return;
+     }
+ 
+  //form the log string
+  snprintf(tmp_str, msg_size,"%s::%s:%s", get_curr_time() , var ,msg);
+  if (to_file) {
+     write_to_logfile(tmp_str);
+    }
+  switch (var) {
+     case INFO:
+         printf("\n%s",tmp_str); 
+     case WARNING:
+         printf("\n%s", tmp_str);
+         //check if # of warning before calling err is required , later
+     case ERROR:
+         printf("\n%s", tmp_str);
+         printf("\nTraceback:");
+         (void) print_func_stack();
+     case FATAL:
+         printf("\n%s", tmp_str);
+         printf("\nTraceback:");
+         (void) print_func_stack();
+ //        (void) kill_the_proc();
+     case DEBUG:
+         print_dbg_log(msg);
+   }
+
+}
+
+void print_dbg_log (char *msg) 
+{
+   char *tmp_str; 
+   if (!msg) {
+      printf("\n NULL string returned for log msg \n");
+      return;
+    }
+   if (dbg_flag) {
+   snprintf(tmp_str, msg_size,"%s::%s:%s", get_curr_time() ,"DBG",msg);
+   if (to_file == 1) {
+      write_to_logfile(tmp_str);
+    }
+   printf("\n%s",tmp_str);
+  }
+
+ }
+
+/*int  main(int argc, char *argv[]) {
+   //fetch dbg_flag and sys_file o/p
+  
+   for (int i =1 ;i <argc ; i++) {
+      if(strcmp((argv+i), "-dbg") == 0) {
+         dbg_flag = 1;
+      }
+      if (strcmp((argv+i), "-syslog_display") == 0) {
+         to_file = 0;
+      }
+    }     
+   return 0;
+}*/
---
+++ debug.h
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<time.h>
+#include<string.h>
+
+//macro defn
+typedef enum {INFO , WARNING , ERROR , FATAL , DEBUG} log_level;
+int dbg_flag = 0;
+int msg_size = 1000;
+int to_file = 1;
+
+//define timestamp
+char *file = "sys_log";
+FILE *fptr;
+
+char* get_curr_time (void);
+void create_log_file (char *file);
+void delete_log_file (void);
+void write_to_logfile(char *);
+void print_func_stack(void);
+void kill_the_proc(void);
+void print_sys_log(char *msg, log_level var);
+void print_dbg_log(char *msg);
