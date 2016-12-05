cb73f1a716510b8b92bc0da5801dabde929bdd93
svadivu <sanmugavadivu@gmail.com>
2016-12-05 18:27:06 +0530

    Add files via upload

--- common.c
+++ common.c
@@ -87,7 +87,8 @@ static int parseStringStructToJson(json_t *obj, Message *msg){
 }
 */
 
-static int parseIntStructToJson(json_t *obj, Message *msg){
+
+/*static int parseIntStructToJson(json_t *obj, Message *msg){
 	int x = 0, i;
 	json_object_set(obj, "event", json_integer(msg->event));
 	json_object_set(obj, "result", json_integer(msg->result));
@@ -100,9 +101,9 @@ static int parseIntStructToJson(json_t *obj, Message *msg){
 	}
 	json_object_set(obj, "data", temp_arr);
 	return SUCCESS;
-}
+}*/
 
-int parseStruct(char **str, Message *msg){
+/*int parseStruct(char **str, Message *msg){
 	json_t *obj = json_object();
 	switch(msg->event){
 		case SERVER_CCLIENT_CONNECTION_ACCEPTED : ;
@@ -121,6 +122,18 @@ int parseStruct(char **str, Message *msg){
 	*str = json_dumps(obj,0);
 	json_decref(obj);
 	return SUCCESS;
+}*/
+int parseStruct(char **str, Message *msg) {
+    char *str1 , *tmp_str;
+    str1 = (char *)malloc(sizeof(msg->event)+sizeof(msg->result)+(msg->client_id)+(msg->data_len)+(sizeof(int)*(msg->data_len)));
+    snprintf(str1 ,size(str1) , "%d %d %d %d ",msg->event , msg->result , msg->client_id , msg->data_len);
+    for (int i=0 ; i < msg->data_len ; i++) {
+        snprintf( tmp_str , sizeof(int)+1 ,"%d " , *((msg->data)+i));
+        strcat(str1, tmp_str);
+      }
+    printf("\n the string is %s ", str1);
+    str = &str1;
+    return 1;
 }
 
 /*
@@ -132,7 +145,7 @@ static int parseJsonToStringStruct(json_t *obj, Message *msg){
 	return SUCCESS;
 }
 */
-
+/*
 static int parseJsonToIntStruct(json_t *obj, Message *msg){
 	int x = 0, i;
 	msg->result = json_integer_value(json_object_get(obj, "result"));
@@ -171,7 +184,7 @@ int parseJson(char *strMsg, Message *msg){
 	json_decref(obj);
 	return SUCCESS;
 }
-
+*/
 
 int populate_and_send_data(int event, int *data, int datalen, int fd, int client_id)
 {
